3ef5f7e8f24e2a3302a2176e219bf8c0a8b1d47c
Serg Pretender <sergdynamite@gmail.com>
2023-04-20 12:05:13 +0300

    home work 7 in process

---
+++ home_work/h_w_7.py
@@ -0,0 +1,92 @@
+import json
+
+QUESTIONS = "questions.json"
+
+
+def load_questions(filename: str) -> dict:
+    """_Load questions from file_
+
+    Args:
+        filename (str): _name of file/ path to file_
+
+    Returns:
+        dict: _dict with questions_
+    """
+
+    with open(filename, encoding='utf-8') as file:
+        return json.load(file)
+
+
+def show_field(questions: dict) -> None:
+    """_Show field_
+
+    Args:
+        questions (dict): _dict with questions_
+    """
+    for category, scores in questions.items():
+        print(f"{str(category).ljust(10)}", end="  ")
+        for score in scores:
+            print(f"{score}", end="   ")
+        print()
+
+
+def parse_input() -> tuple:
+    """_Parse user input_
+
+    Returns:
+        tuple: _category, price_
+    """
+    category, price = input("-> ").split()
+    return category.title(), price
+
+
+## def is_question in base(questious):
+
+
+def show_questions(questions: dict, category: str, number: str):
+
+    if category not in questions:
+        print("Невірно обрана категорія")
+        return questions, None
+
+    for price, quest in questions[category].items():
+        if number == price:
+            print(f"Слово {quest['question']} в перекладі означає ...")
+            return questions, quest['answer']
+        else:
+            print('Немає такої ціни')
+            return questions, None
+
+
+def main():
+    scores = 0
+    correct_answers = 0
+    fail_answers = 0
+    questions = load_questions(QUESTIONS)
+    while True:
+        show_field(questions)
+        print("Виберіть тему та ціну")
+        try:
+            user_category, user_price = parse_input()
+        except Exception as e:
+            print('Помилка воду', e)
+            continue
+        questions, question = show_questions(questions, user_category,
+                                             user_price)
+        if question is None:
+            continue
+        user_answer = input("-> ").lower()
+        if user_answer == question:
+            print("Правильно")
+            scores += int(user_price)
+            correct_answers += 1
+        else:
+            fail_answers += 1
+            scores -= int(user_price)
+            print(f"Неправильно, правильна відповідь \
+            {questions[user_category][user_price]['answer']}, {-int(user_price)}\
+                , Ваш рахунок {scores}")
+
+
+if __name__ == '__main__':
+    main()
