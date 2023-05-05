f517a80d93d4a2b6f3a85b52c5161be1585f2302
Serg Pretender <sergdynamite@gmail.com>
2023-05-05 20:36:29 +0300

    home work 11 finish

--- home_work/11/app_11.py
+++ home_work/11/app_11.py
@@ -13,11 +13,7 @@ candidates = load_candidates_from_json(PATH_TO_CANDIDATE_DATA)
 
 @app.route('/')
 def main_page():
-    candidate_str = '<h1>All candidates</h1>'
-    for candidate in candidates:
-        candidate_name = candidate.get('name')
-        candidate_str += f"<p><a href='/candidate/{candidate_name}'>{candidate_name}</a></p>"
-    return candidate_str
+    return render_template('list.html', candidates=candidates)
 
 
 @app.route('/candidate/<x>')
@@ -25,6 +21,27 @@ def candidate_info(x):
     for candidate in candidates:
         if x in candidate.values():
             return render_template('single.html', candidate=candidate)
+
+
+@app.route('/search/<candidate_name>')
+def search_candidate(candidate_name):
+    candidates_list = []
+    for candidate in candidates:
+        search_candidate = candidate.get("name")
+        if search_candidate.split()[0] == candidate_name:
+            candidates_list.append(search_candidate)
+
+    return render_template('search.html', count=len(candidates_list), candidates=candidates_list)
+
+
+@app.route('/skill/<skill_name>')
+def skills(skill_name):
+    candidates_with_skill = []
+    for candidate in candidates:
+        skills = candidate.get("skills").lower().split(', ')
+        if skill_name.lower() in skills:
+            candidates_with_skill.append(candidate.get('name'))
+    return render_template('skill.html', count=len(candidates_with_skill), candidates=candidates_with_skill, skill=skill_name)
 
 
 if __name__ == '__main__':
