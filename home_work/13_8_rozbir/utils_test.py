5312bfd89aff9f81a7a314bc89d15353d9a61070
Serg Pretender <sergdynamite@gmail.com>
2023-06-02 12:14:09 +0300

    розділ 2, розбір домашки 13  частина 2

--- home_work/13_8_rozbir/utils_test.py
+++ home_work/13_8_rozbir/utils_test.py
@@ -2,6 +2,13 @@ import pytest
 from utils import has_r
 
 
-def test_has_r():
+def test_has_r_with():
     assert has_r("world") is True, "has R"
-    assert has_r("wood") is False, "do'nt has R"
+
+
+def test_has_r_without():
+    pass
+
+
+def test_has_r_wrong_type():
+    pass
