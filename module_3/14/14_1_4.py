3a5806f0790fc6f7f9f4e65bf3e67fb9f1d38833
Serg Pretender <sergdynamite@gmail.com>
2023-06-15 23:37:00 +0300

    розділ 3 дз початок

--- module_3/14/14_1_4.py
+++ module_3/14/14_1_4.py
@@ -4,9 +4,9 @@ import sqlite3
 with sqlite3.connect("cars_1.db") as connection:
     cursor = connection.cursor()
     sql_query = ("""
-    SELECT "price", "model" FROM cars
-    WHERE "model" LIKE "V%"
-    ORDER BY "price" DESC
+    SELECT "price", "model" 
+    FROM cars
+    GROUP BY "model"
     """)
     cursor.execute(sql_query)
     # print(cursor.fetchone())
