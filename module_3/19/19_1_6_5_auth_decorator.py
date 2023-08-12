87f67b48eb8941377f9110be74fa8ae439678391
Serg Pretender <sergdynamite@gmail.com>
2023-08-12 10:51:55 +0300

    19_1_6 1

--- module_3/19/19_1_6_5_auth_decorator.py
+++ module_3/19/19_1_6_5_auth_decorator.py
@@ -1,11 +1,23 @@
+import jwt
 from flask import Flask, abort, request
-
 from flask_restx import Api, Resource
 
 
-def login_required(func):
+algo = "HS256"
+secret = "s3cR$et"
+
+
+def auth_required(func):
     def wrapper(*args, **kwargs):
         if "Authorization" not in request.headers:
+            abort(401)
+            
+        data = request.headers.get('Authorization')
+        token = data.split("Bearer ")[-1]
+        try:
+            jwt.decode(token, secret, algorithms=[algo])
+        except Exception as e:
+            print("JWT Decode Exception", e)
             abort(401)
         return func(*args, **kwargs)
     return wrapper
@@ -21,7 +33,7 @@ class BooksView(Resource):
     def get(self):
         return "Work", 200
 
-    @login_required
+    @auth_required
     def post(self):
         return '', 201
 
